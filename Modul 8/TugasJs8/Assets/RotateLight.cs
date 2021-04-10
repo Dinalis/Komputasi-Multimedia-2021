effae3f201f13dc8007a8932fab94356a633b1a1
Dinalis <lisuardidina@gmail.com>
2021-04-10 14:00:36 +0700

    Upload Modul 8 dan UTS

---
+++ Modul 8/TugasJs8/Assets/RotateLight.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLight : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    public float speed = -1.0f;
+    void Update()
+    {
+        transform.Rotate(Vector3.right * speed * Time.deltaTime);
+    }
+}
